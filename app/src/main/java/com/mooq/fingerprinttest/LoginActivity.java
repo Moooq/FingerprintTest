377cbdda6e73b0fbfeac94b191c6942e8ae0ba17
moqian <jam.mooq@foxmail.com>
2019-05-22 10:03:57 +0800

    Initial commit

---
+++ app/src/main/java/com/mooq/fingerprinttest/LoginActivity.java
@@ -0,0 +1,117 @@
+package com.mooq.fingerprinttest;
+
+import android.annotation.TargetApi;
+import android.app.KeyguardManager;
+import android.content.Intent;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.Build;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.security.KeyStore;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+public class LoginActivity extends AppCompatActivity {
+
+	private static final String DEFAULT_KEY_NAME = "default_key";
+
+	KeyStore keyStore;
+
+	Button btnFp;
+
+	Cipher cipher;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		btnFp = (Button) findViewById(R.id.btn_fp);
+		if (supportFingerprint()) {
+			initKey();
+			initCipher();
+		}
+		btnFp.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (supportFingerprint()&&cipher!=null){
+					showFingerPrintDialog(cipher);
+				}
+			}
+		});
+	}
+
+	public boolean supportFingerprint() {
+		if (Build.VERSION.SDK_INT < 23) {
+			Toast.makeText(this, "您的系统版本过低，不支持指纹功能", Toast.LENGTH_SHORT).show();
+			return false;
+		} else {
+			KeyguardManager keyguardManager = getSystemService(KeyguardManager.class);
+			FingerprintManager fingerprintManager = getSystemService(FingerprintManager.class);
+			if (!fingerprintManager.isHardwareDetected()) {
+				Toast.makeText(this, "您的手机不支持指纹功能", Toast.LENGTH_SHORT).show();
+				return false;
+			} else if (!keyguardManager.isKeyguardSecure()) {
+				Toast.makeText(this, "您还未设置锁屏，请先设置锁屏并添加一个指纹", Toast.LENGTH_SHORT).show();
+				return false;
+			} else if (!fingerprintManager.hasEnrolledFingerprints()) {
+				Toast.makeText(this, "您至少需要在系统设置中添加一个指纹", Toast.LENGTH_SHORT).show();
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@TargetApi(23)
+	private void initKey() {
+		try {
+			keyStore = KeyStore.getInstance("AndroidKeyStore");
+			keyStore.load(null);
+			KeyGenerator keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+			KeyGenParameterSpec.Builder builder = new KeyGenParameterSpec.Builder(DEFAULT_KEY_NAME,
+					KeyProperties.PURPOSE_ENCRYPT |
+							KeyProperties.PURPOSE_DECRYPT)
+					.setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+					.setUserAuthenticationRequired(true)
+					.setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7);
+			keyGenerator.init(builder.build());
+			keyGenerator.generateKey();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@TargetApi(23)
+	private void initCipher() {
+		try {
+			SecretKey key = (SecretKey) keyStore.getKey(DEFAULT_KEY_NAME, null);
+			cipher = Cipher.getInstance(KeyProperties.KEY_ALGORITHM_AES + "/"
+					+ KeyProperties.BLOCK_MODE_CBC + "/"
+					+ KeyProperties.ENCRYPTION_PADDING_PKCS7);
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			showFingerPrintDialog(cipher);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void showFingerPrintDialog(Cipher cipher) {
+		FingerprintDialogFragment fragment = new FingerprintDialogFragment();
+		fragment.setCipher(cipher);
+		fragment.show(getSupportFragmentManager(), "fingerprint");
+	}
+
+	public void onAuthenticated() {
+		Intent intent = new Intent(this, MainActivity.class);
+		startActivity(intent);
+		finish();
+	}
+
+}
